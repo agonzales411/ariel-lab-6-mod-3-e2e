74a0ca382f81c47f83f7e50aa5cb70c0e5bad8ed
agonzales411 <agv417@gmail.com>
2023-03-23 21:33:03 -0400

    framework

--- README.md
+++
@@ -1,2 +0,0 @@
-# ariel-lab-6-mod-3-e2e
-Framework over Playwright with Cucumber JS
---
+++ config.app.ts
@@ -0,0 +1,9 @@
+export const TESTDATA = {
+    email: '',
+    password: '',
+    url: 'https://todoist.com/auth/login',
+    apiUrl: 'https://api.todoist.com/rest/v2/',
+    apiToken: '',
+}
+
+//console.log(process.env.JAVA_HOME);
